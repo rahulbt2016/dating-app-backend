d22f1a85e8bb4a150fc6edc0545ad71d2ce781a8
Rahul Tiwari <rahul.tiwari@fdmgroup.com>
2025-05-04 18:24:23 -0400

    Get users API endpoints

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    [Key]
+    public int Id { get; set; }
+    public required string UserName { get; set; }
+
+}
